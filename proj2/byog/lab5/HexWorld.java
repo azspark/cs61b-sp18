4ce6db2a5cb54a2a756c000a4eea11b60ed208be
azspark <zz1254657569@gmail.com>
2019-12-08 01:08:48 +0800

    lab5 done

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -5,11 +5,159 @@ import static org.junit.Assert.*;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
-
 import java.util.Random;
+
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    private static TETile randomTETile() {
+        Random r = new Random();
+        int tileNum = r.nextInt(4);
+        switch (tileNum) {
+            case 0: return Tileset.TREE;
+            case 1: return Tileset.FLOWER;
+            case 2: return Tileset.MOUNTAIN;
+            case 3: return Tileset.SAND;
+            default: return Tileset.NOTHING;
+        }
+    }
+
+    /**
+     * Computes the width of row i for a size s hexagon.
+     * @param s The size of the hex.
+     * @param i The row number where i = 0 is the bottom row.
+     * @return
+     */
+    public static int hexRowWidth(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+
+        return s + 2 * effectiveI;
+    }
+
+    /**
+     * Computesrelative x coordinate of the leftmost tile in the ith
+     * row of a hexagon, assuming that the bottom row has an x-coordinate
+     * of zero. For example, if s = 3, and i = 2, this function
+     * returns -2, because the row 2 up from the bottom starts 2 to the left
+     * of the start position, e.g.
+     *   xxxx
+     *  xxxxxx
+     * xxxxxxxx
+     * xxxxxxxx <-- i = 2, starts 2 spots to the left of the bottom of the hex
+     *  xxxxxx
+     *   xxxx
+     *
+     * @param s size of the hexagon
+     * @param i row num of the hexagon, where i = 0 is the bottom
+     * @return
+     */
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return -effectiveI;
+    }
+
+    /** Adds a row of the same tile.
+     * @param world the world to draw on
+     * @param p the leftmost position of the row
+     * @param width the number of tiles wide to draw
+     * @param t the tile to draw
+     */
+    public static void addRow(TETile[][] world, Position p, int width, TETile t) {
+        for (int xi = 0; xi < width; xi += 1) {
+            int xCoord = p.x + xi;
+            int yCoord = p.y;
+            world[xCoord][yCoord] = TETile.colorVariant(t, 32, 32, 32, new Random());
+        }
+    }
+
+    /**
+     * Adds a hexagon to the world.
+     * @param world the world to draw on
+     * @param p the bottom left coordinate of the hexagon
+     * @param s the size of the hexagon
+     */
+    public static void addHexagon(TETile[][] world, Position p, int s) {
+
+        TETile t = randomTETile();
+
+        if (s < 2) {
+            throw new IllegalArgumentException("Hexagon must be at least size 2.");
+        }
+
+        // hexagons have 2*s rows. this code iterates up from the bottom row,
+        // which we call row 0.
+        for (int yi = 0; yi < 2 * s; yi += 1) {
+            int thisRowY = p.y + yi;
+
+            int xRowStart = p.x + hexRowOffset(s, yi);
+            Position rowStartP = new Position(xRowStart, thisRowY);
+
+            int rowWidth = hexRowWidth(s, yi);
+
+            addRow(world, rowStartP, rowWidth, t);
+        }
+    }
+
+    public static void addColumnHexagons(int size, int number, TETile[][] world, Position start) {
+        Position curPosition = start;
+        for (int i = 0; i < number; i++) {
+            if (i != 0) {
+                curPosition = new Position(curPosition.x, curPosition.y + size * 2);
+            }
+            addHexagon(world, curPosition, size);
+        }
+    }
+
+    public static void drawTesselationHexagons(int size, TETile[][] world) {
+        // calculate the first position
+        int initX = size - 1;
+        int initY = 2 * size;// 2 * size + 1 - 1
+        Position curPosition = new Position(initX, initY);
+        addColumnHexagons(size, 3, world, curPosition);
+        curPosition = downRightPosition(curPosition, size);
+        addColumnHexagons(size, 4, world, curPosition);
+        curPosition = downRightPosition(curPosition, size);
+        addColumnHexagons(size, 5, world, curPosition);
+        curPosition = upRightPosition(curPosition, size);
+        addColumnHexagons(size, 4, world, curPosition);
+        curPosition = upRightPosition(curPosition, size);
+        addColumnHexagons(size, 3, world, curPosition);
+    }
+
+    public static Position downRightPosition(Position p, int size) {
+        return new Position(p.x + 2 * size-1, p.y - size);
+    }
+
+    public static Position upRightPosition(Position p, int size) {
+        return new Position(p.x + 2 * size-1, p.y + size);
+    }
+
+    public static void main(String args[]) {
+        int size = 10;
+
+        int lenX = 3 * (3 * size - 2) + 2 * size;
+        int lenY = (2 * size) * 5 ;
+        TERenderer ter  = new TERenderer();
+        ter.initialize(lenX, lenY);
+        TETile[][] world = new TETile[lenX][lenY];
+        for (int i = 0; i < lenX; i ++) {
+            for (int j = 0; j < lenY; j++) {
+                world[i][j] = Tileset.NOTHING;
+            }
+        }
+
+        drawTesselationHexagons(size, world);
+
+        ter.renderFrame(world);
+//        hw.addHexagon(3);
+    }
 }
